c420416e8833d2b71fdf2cf254d61968fad1b4a4
ofwebdev <nkomorfaruk@gmail.com>
2023-03-17 11:58:00 +0600

    Initial setup

---
+++ system/color.js
@@ -0,0 +1,68 @@
+// primary.main
+// secondary.main
+// error.main
+// warning.main
+// info.main
+// success.main
+// text.primary
+// text.secondary
+// text.disabled
+
+const MuiColors = {
+  primary: {
+    lighter: "#C8FACD",
+    light: "#5BE584",
+    main: "#00AB55",
+    dark: "#007B55",
+    darker: "#005249",
+  },
+  secondary: {
+    lighter: "#D6E4FF",
+    light: "#84A9FF",
+    main: "#3366FF",
+    dark: "#1939B7",
+    darker: "#091A7A",
+  },
+  info: {
+    lighter: "#CAFDF5",
+    light: "#61F3F3",
+    main: "#00B8D9",
+    dark: "#006C9C",
+    darker: "#003768",
+  },
+
+  success: {
+    lighter: "#D8FBDE",
+    light: "#86E8AB",
+    main: "#36B37E",
+    dark: "#1B806A",
+    darker: "#0A5554",
+  },
+
+  warning: {
+    lighter: "#FFF5CC",
+    light: "#FFD666",
+    main: "#FFAB00",
+    dark: "#B76E00",
+    darker: "#7A4100",
+  },
+
+  error: {
+    lighter: "#FFE9D5",
+    light: "#FFAC82",
+    main: "#FF5630",
+    dark: "#B71D18",
+    darker: "#7A0916",
+  },
+  grey: {
+    100: "#F9FAFB",
+    200: "#F4F6F8",
+    300: "#DFE3E8",
+    400: "#C4CDD5",
+    500: "#919EAB",
+    600: "#637381",
+    700: "#454F5B",
+    800: "#212B36",
+    900: "#161C24",
+  },
+};
